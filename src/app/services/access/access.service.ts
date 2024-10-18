320c4bd5f1017b48eadfe9628fe7a47608fafafa
Nahuel Ignacio Quilpatay - 113935 <113935@tecnicatura.frc.utn.edu.ar>
2024-10-18 12:55:43 -0300

    template3

---
+++ src/app/services/access/access.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AccessRecordResponse } from '../../models/accesses/access-record-response.model';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccessService {
+  private baseUrl = 'http://localhost:63478/visit-access';
+
+  constructor(private http: HttpClient) {}
+
+  getAccessRecords(
+    accessType: string,
+    paramsObj: any
+  ): Observable<AccessRecordResponse[]> {
+    let params = new HttpParams();
+
+    Object.keys(paramsObj).forEach((key) => {
+      if (paramsObj[key] !== undefined && paramsObj[key] !== '') {
+        params = params.append(key, paramsObj[key]);
+      }
+    });
+
+    return this.http.get<AccessRecordResponse[]>(`${this.baseUrl}`, { params });
+  }
+}
